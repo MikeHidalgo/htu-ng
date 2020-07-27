a7fdf54c11f863379a7dde566fb6a8a6c3ae1f7e
Mike <mike.hidalgo@outlook.com>
2020-07-26 23:03:13 -0700

    first try at full dmi table.

--- cli/models/dmi.go
+++ cli/models/dmi.go
@@ -65,7 +65,7 @@ func DMI() {
     statement, _ = database.Prepare("INSERT INTO dmi (id, component, output) VALUES (?, ?, ?)")
 
     for code, title := range CodeTable() {
-        statement.Exec(code, title, RunCommand("dmidecode", [2]string{"-t", strconv.Itoa(code)}))
+        statement.Exec(code, title, RunCommand("dmidecode", []string{"-t", strconv.Itoa(code)}))
     }
 
     rows, _ := database.Query("SELECT id, component, output FROM dmi")
